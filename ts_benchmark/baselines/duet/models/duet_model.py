9129456546ae1717e3c2f4c4f3df1420db053177
QSY <3234946025@qq.com>
2025-06-15 12:47:43 +0800

    dutmedel,py修改为原本的duet原代码

--- ts_benchmark/baselines/duet/models/duet_model.py
+++ ts_benchmark/baselines/duet/models/duet_model.py
@@ -41,67 +41,13 @@ class DUETModel(nn.Module):
         # x: [batch_size, seq_len, n_vars]
         if self.CI:
             channel_independent_input = rearrange(input, 'b l n -> (b n) l 1')
-            ###方案一
-            ###
-            #这里加下采样,并把下采样结果变成channel_independent_input的形状,对于每一个长度的进行cluster                    
-            #下面是添加进来的多尺度模块，开始修改
 
-            #如果应用未来特征..，但是我们没有用到
-            if self.use_future_temporal_feature:
-                if self.channel_independence == 1:
-                    B, T, N = x_enc.size()
-                    x_mark_dec = x_mark_dec.repeat(N, 1, 1)
-                    self.x_mark_dec = self.enc_embedding(None, x_mark_dec)
-                else:
-                    self.x_mark_dec = self.enc_embedding(None, x_mark_dec)
-            #开始下采样
-            x_enc, x_mark_enc = self.__multi_scale_process_inputs(x_enc, x_mark_enc)##下采样，输入时间序列BLN，输出[BLN;BLN;..]
+            reshaped_output, L_importance = self.cluster(channel_independent_input)
 
-            x_list = []
-            x_mark_list = []
-            if x_mark_enc is not None:
-                for i, x, x_mark in zip(range(len(x_enc)), x_enc, x_mark_enc):
-                    B, T, N = x.size()
-                    x = self.normalize_layers[i](x, 'norm')
-                    if self.channel_independence == 1:
-                        x = x.permute(0, 2, 1).contiguous().reshape(B * N, T, 1)
-                        x_mark = x_mark.repeat(N, 1, 1)
-                    x_list.append(x)
-                    x_mark_list.append(x_mark)
-            else:
-                for i, x in zip(range(len(x_enc)), x_enc, ):
-                    B, T, N = x.size()
-                    x = self.normalize_layers[i](x, 'norm')#归一化
-                    if self.channel_independence == 1:         
-                        x = x.permute(0, 2, 1).contiguous().reshape(B * N, T, 1)
-                    x_list.append(x)
-    ###
-
-            #此处要将x_list作为输入输进去，检查x_list的格式以及channel_independent_input的格式
-            #检查x_mark_enc的处理
-            for i in x_list:
-
-
-
-            ###
-            #开始进行MOE
-                reshaped_output, L_importance = self.cluster(channel_independent_input)
-            ###
-            #这里加尺度混合,整合为多尺度混合后的时序特征,形状要是(bn) l 1
-            
-
-
-            ###
-        ###
-        #方案二，在每个MOE里专家接受到任务后开始下采样，然后分别对每个进行提取，再进行混合输出半成品
-        ###
-
-        ###修改结束,        
             temporal_feature = rearrange(reshaped_output, '(b n) l 1 -> b l n', b=input.shape[0])
 
-        else:#这边是通道不独立的分支，未作多尺度的修改
+        else:
             temporal_feature, L_importance = self.cluster(input)
-
 
         # B x d_model x n_vars -> B x n_vars x d_model
         temporal_feature = rearrange(temporal_feature, 'b d n -> b n d')
