b8343577d99197da578ab18759a8af4319dc1b76
QSY <3234946025@qq.com>
2025-06-15 14:20:32 +0800

    把代码确定为了与各方案设想一致的样子，起点终于摆正了

--- ts_benchmark/baselines/duet/models/duet_model.py
+++ ts_benchmark/baselines/duet/models/duet_model.py
@@ -37,71 +37,25 @@ class DUETModel(nn.Module):
 
         self.linear_head = nn.Sequential(nn.Linear(config.d_model, config.pred_len), nn.Dropout(config.fc_dropout))
 
-    def forward(self, input):
+def forward(self, input):
+        #方案一：在每个moe之前完成下采样，并将每个尺度的都送进moe进行处理，但问题是每个moe内部就进行趋势分解和合并了，输出了完整的，没法进行趋势和季节分别尺度混合在混合。
+        #我有一个大胆的想法，moe内部混合完之后，我外部再进行混合，然后再进行多尺度混合，但是我不是很确定，原本的是对谁进行季节和趋势的分解？也是完整的吗？是的，是完整的，直接输入归一化后的各种尺度组成的列表。然后直接输入到趋势分解函数中。
+        #目前采用的是方案二，即在每个moe内部进行
+        #如何实现跨分支的实验记录？
+        #实际上timemixser后面还有一部分代码没看，不知道作用
+        #启示：下一次看代码做思维导图的时候要详细，要把内容尽可能地完善，各种情况下的，不然我确实后面没有心力再去看了，并且的确有可能遇到
+        #如何实验自己的更改是否有效，需要把代码提交上去再跑，还是暂存就可以了？另外我跑实验本地没法跑linux命令，想起来了好像可以。修改下命令行？真的吗，我怎么记得上一次在linux上也需要修改？
         # x: [batch_size, seq_len, n_vars]
+        #发现个问题为什么切换分支之后，打开的代码还是原分支的修改没有回到该分支的版本？--没有commit!
         if self.CI:
             channel_independent_input = rearrange(input, 'b l n -> (b n) l 1')
-            ###方案一
-            ###
-            #这里加下采样,并把下采样结果变成channel_independent_input的形状,对于每一个长度的进行cluster                    
-            #下面是添加进来的多尺度模块，开始修改
 
-            #如果应用未来特征..，但是我们没有用到
-            if self.use_future_temporal_feature:
-                if self.channel_independence == 1:
-                    B, T, N = x_enc.size()
-                    x_mark_dec = x_mark_dec.repeat(N, 1, 1)
-                    self.x_mark_dec = self.enc_embedding(None, x_mark_dec)
-                else:
-                    self.x_mark_dec = self.enc_embedding(None, x_mark_dec)
-            #开始下采样
-            x_enc, x_mark_enc = self.__multi_scale_process_inputs(x_enc, x_mark_enc)##下采样，输入时间序列BLN，输出[BLN;BLN;..]
+            reshaped_output, L_importance = self.cluster(channel_independent_input)
 
-            x_list = []
-            x_mark_list = []
-            if x_mark_enc is not None:
-                for i, x, x_mark in zip(range(len(x_enc)), x_enc, x_mark_enc):
-                    B, T, N = x.size()
-                    x = self.normalize_layers[i](x, 'norm')
-                    if self.channel_independence == 1:
-                        x = x.permute(0, 2, 1).contiguous().reshape(B * N, T, 1)
-                        x_mark = x_mark.repeat(N, 1, 1)
-                    x_list.append(x)
-                    x_mark_list.append(x_mark)
-            else:
-                for i, x in zip(range(len(x_enc)), x_enc, ):
-                    B, T, N = x.size()
-                    x = self.normalize_layers[i](x, 'norm')#归一化
-                    if self.channel_independence == 1:         
-                        x = x.permute(0, 2, 1).contiguous().reshape(B * N, T, 1)
-                    x_list.append(x)
-    ###
-
-            #此处要将x_list作为输入输进去，检查x_list的格式以及channel_independent_input的格式
-            #检查x_mark_enc的处理
-            for i in x_list:
-
-
-
-            ###
-            #开始进行MOE
-                reshaped_output, L_importance = self.cluster(channel_independent_input)
-            ###
-            #这里加尺度混合,整合为多尺度混合后的时序特征,形状要是(bn) l 1
-            
-
-
-            ###
-        ###
-        #方案二，在每个MOE里专家接受到任务后开始下采样，然后分别对每个进行提取，再进行混合输出半成品
-        ###
-
-        ###修改结束,        
             temporal_feature = rearrange(reshaped_output, '(b n) l 1 -> b l n', b=input.shape[0])
 
-        else:#这边是通道不独立的分支，未作多尺度的修改
+        else:
             temporal_feature, L_importance = self.cluster(input)
-
 
         # B x d_model x n_vars -> B x n_vars x d_model
         temporal_feature = rearrange(temporal_feature, 'b d n -> b n d')
