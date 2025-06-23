699026f5d562a07be8d6b27a95d984cc9b8b8df2
QSY <3234946025@qq.com>
2025-06-23 15:02:30 +0800

    发现主文件存在一开始的遗留问题，修正了导入的文件路径错误。关键：看标黄的部分！不要忽视

--- ts_benchmark/baselines/duet/layers/linear_extractor_cluster.py
+++ ts_benchmark/baselines/duet/layers/linear_extractor_cluster.py
@@ -3,15 +3,13 @@ import torch.nn as nn
 from torch.distributions.normal import Normal
 from .linear_pattern_extractor import Linear_extractor as expert
 from .distributional_router_encoder import encoder
-from ..layers.RevIN import RevIN
+from .RevIN import RevIN
 from einops import rearrange
 ######添加了DFT_series_decomp
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from layers.Autoformer_EncDec import series_decomp
-from layers.Embed import DataEmbedding_wo_pos
-from layers.StandardNorm import Normalize
+
+from .Autoformer_EncDec import series_decomp
+from .Embed import DataEmbedding_wo_pos
+
 
 class DFT_series_decomp(nn.Module):
     """
@@ -32,77 +30,7 @@ class DFT_series_decomp(nn.Module):
         x_trend = x - x_season
         return x_season, x_trend
 
-#添加了PastDecomposableMixing，没有同步import
 
-class PastDecomposableMixing(nn.Module):
-    def __init__(self, configs):
-        super(PastDecomposableMixing, self).__init__()
-        self.seq_len = configs.seq_len
-        self.pred_len = configs.pred_len
-        self.down_sampling_window = configs.down_sampling_window
-
-        self.layer_norm = nn.LayerNorm(configs.d_model)
-        self.dropout = nn.Dropout(configs.dropout)
-        self.channel_independence = configs.channel_independence
-
-        if configs.decomp_method == 'moving_avg':
-            self.decompsition = series_decomp(configs.moving_avg)
-        elif configs.decomp_method == "dft_decomp":
-            self.decompsition = DFT_series_decomp(configs.top_k)
-        else:
-            raise ValueError('decompsition is error')
-
-        if configs.channel_independence == 0:
-            self.cross_layer = nn.Sequential(
-                nn.Linear(in_features=configs.d_model, out_features=configs.d_ff),
-                nn.GELU(),
-                nn.Linear(in_features=configs.d_ff, out_features=configs.d_model),
-            )
-
-        # Mixing season
-        self.mixing_multi_scale_season = MultiScaleSeasonMixing(configs)
-
-        # Mxing trend
-        self.mixing_multi_scale_trend = MultiScaleTrendMixing(configs)
-
-        self.out_cross_layer = nn.Sequential(
-            nn.Linear(in_features=configs.d_model, out_features=configs.d_ff),
-            nn.GELU(),
-            nn.Linear(in_features=configs.d_ff, out_features=configs.d_model),
-        )
-
-    def forward(self, x_list):
-        length_list = []
-        for x in x_list:
-            _, T, _ = x.size()
-            length_list.append(T)
-
-        # Decompose to obtain the season and trend
-        season_list = []
-        trend_list = []
-        for x in x_list:
-            season, trend = self.decompsition(x)
-            if self.channel_independence == 0:
-                season = self.cross_layer(season)
-                trend = self.cross_layer(trend)
-            season_list.append(season.permute(0, 2, 1))
-            trend_list.append(trend.permute(0, 2, 1))
-
-        # bottom-up season mixing
-        out_season_list = self.mixing_multi_scale_season(season_list)
-        # top-down trend mixing
-        out_trend_list = self.mixing_multi_scale_trend(trend_list)
-
-        out_list = []
-        for ori, out_season, out_trend, length in zip(x_list, out_season_list, out_trend_list,
-                                                      length_list):
-            out = out_season + out_trend
-            if self.channel_independence:
-                out = ori + self.out_cross_layer(out)
-            out_list.append(out[:, :length, :])
-        return out_list
-    
-############
 class SparseDispatcher(object):
     """Helper for implementing a mixture of experts.
     The purpose of this class is to create input mini-batches for the
